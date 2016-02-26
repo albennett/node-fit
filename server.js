32e9df63119ab62dba7f4303e32c1c25d9f4d296
Mike Ulvila <mikeulvila@me.com>
2016-02-26 10:27:55 -0600

    added sql database

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const sqlite3 = require('sqlite3');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-// const db = new sqlite3.Database('./db/node-fit.sqlite');
+const db = new sqlite3.Database('./db/node-fit.sqlite');
 
 app.get('/', (req, res) => {
   res.send('hello');
