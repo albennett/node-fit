808739e8332e5b59b4ca3fc8add70e0620497677
Amy Lee <amyleeannebennett@gmail.com>
2016-02-26 10:20:57 -0600

    server is running

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const sqlite3 = require('sqlite3');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+// const db = new sqlite3.Database('./db/node-fit.sqlite');
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server listening on port: ${PORT}`);
+});
